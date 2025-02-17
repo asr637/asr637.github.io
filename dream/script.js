20fda679af0cca4860ccd5569955de8bffce6ae0
asr637 <aakifshahbazrattu7043@gmail.com>
2025-02-18 01:19:19 +0400

    x

--- dream/script.js
+++ dream/script.js
@@ -2,8 +2,8 @@ function navigateToFilmPage() {
     // Add fade-out effect before navigating
     document.querySelector('.landing-page').style.animation = 'fadeOut 0.5s forwards';
     
-    // After animation, navigate to the film page (you can change the URL accordingly)
+    // After animation, navigate to the film page 
     setTimeout(() => {
-        window.location.href = 'film-page.html'; // Change this URL to your film page's URL
+        window.location.href = 'film-page.html'; 
     }, 500);
 }
